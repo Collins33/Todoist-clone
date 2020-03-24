d01ffcde9c8d2675258c85abccb45f2f6891e6d1
collins njau <collinsnjau39@gmail.com>
2020-03-24 19:55:35 +0300

    Add side select styling

--- src/components/sideSelect/index.js
+++ src/components/sideSelect/index.js
@@ -4,12 +4,12 @@ import "./index.css";
 export const SideSelect = ({ selectItems }) => {
   const displaySelectBar = selectItems.map((item, index) => (
     <div className="side_bar_card" key={index}>
-      <div>{item.number}</div>
-      <div>{item.name}</div>
+      <div className="card_item">{item.number}</div>
+      <div className="card_item">{item.name}</div>
       <div className="elipsis_container">
-        <h6>.</h6>
-        <h6>.</h6>
-        <h6>.</h6>
+        <h5>.</h5>
+        <h5>.</h5>
+        <h5>.</h5>
       </div>
     </div>
   ));
