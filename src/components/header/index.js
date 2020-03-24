4830c364ab3f74ecef0e1279e95ed734c0218472
collins njau <collinsnjau39@gmail.com>
2020-03-24 17:40:00 +0300

    Add header - add dashboard header - add styling for the header

---
+++ src/components/header/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./index.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header_component">
+        <div>
+          <button>Add assessment +</button>
+        </div>
+        <div></div>
+        <div>24k/24k Patients seen</div>
+        <div className="profile_container">
+          <div>Collins Njau </div>
+          <div>collinsnjau39@gmail.com</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
