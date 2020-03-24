e89e6a9344e7c9cf131393887a0891cb1408c533
collins njau <collinsnjau39@gmail.com>
2020-03-24 22:10:46 +0300

    Fix the ellipsis styling

--- src/components/ellipses/index.js
+++ src/components/ellipses/index.js
@@ -1,11 +1,13 @@
 import React from "react";
+import "./index.css";
 
 export const Ellipsis = () => {
   return (
-    <div className="elipsis_container">
-      <h5>.</h5>
-      <h5>.</h5>
-      <h5>.</h5>
-    </div>
+    // <div className="elipsis_container">
+    //   <h5>.</h5>
+    //   <h5>.</h5>
+    //   <h5>.</h5>
+    // </div>
+    <div className="ellipsis"></div>
   );
 };
