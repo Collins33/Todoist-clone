487dcb89bd408e4df42efa673bc865248183075f
collins njau <collinsnjau39@gmail.com>
2020-03-25 21:12:14 +0300

    Add the parent component for the progress bar

---
+++ src/components/progressbar/filler.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Filler = props => {
+  return <div className="filler"></div>;
+};
+
+export default Filler;
---
+++ src/components/progressbar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Filler } from "./filler";
+
+const ProgressBar = props => {
+  return (
+    <div className="progress-bar">
+      <Filler />
+    </div>
+  );
+};
+
+export default ProgressBar;
