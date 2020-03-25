bf78051cc5bc1cabb977f9566c1f10ff463f909d
collins njau <collinsnjau39@gmail.com>
2020-03-25 22:53:25 +0300

    Add header image

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ProgressBar from "../progressbar/index";
+import image from "../../images/CollinsNjau_2.jpg";
 import "./index.css";
 
 class Header extends Component {
@@ -14,9 +15,12 @@ class Header extends Component {
           <ProgressBar />
           24k/24k Patients seen
         </div>
-        <div className="profile_container">
-          <div>Collins Njau </div>
-          <div>collinsnjau39@gmail.com</div>
+        <div className="profile_details_container">
+          <div className="profile_container">
+            <div>Collins Njau </div>
+            <div>collinsnjau39@gmail.com</div>
+          </div>
+          <img src={image} alt="Avatar"></img>
         </div>
       </div>
     );
